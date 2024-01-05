ca1eba3e5bf4e4ecc82e3995f122ff3cd33dd184
m.r <mr@example.com>
2024-01-05 13:25:30 -0800

    v3

--- body.js
+++ body.js
@@ -1,27 +1,24 @@
-const { exec } = require("child_process");
-exec("git log -p -1 -- package.json", (err, stdout, stderr) => {
-  if (err) {
-    // node couldn't execute the command
-    return;
-  }
+const { exec, execSync } = require("child_process");
+let result = execSync("git log -p -1 -- package.json");
+let stdout = result.toString();
+// console.log()
+// (err, stdout, stderr) => {
+//   if (err) {
+//     // node couldn't execute the command
+//     return;
+//   }
 
-  // the *entire* stdout and stderr (buffered)
-  //   console.log(`stdout: ${stdout}`);
-  let x = "*************************";
-  let pos = stdout.replace(/@@[\n\s\S]*?@@/, x);
-  let res = pos.substring(pos.indexOf(x) + x.length + 1).replace(/@@[\n\s\S]*?@@/, "");
-  console.log(res.match(/\+.*/g).reduce((result,current)=>{
-return result+"\n"+current.substring(1)
-  },""));
-  //   console.log(`stderr: ${stderr}`);
-});
-`
- {
-   "name": "github-action",
-   "version": "1.0.1",
--  "description": "",
-+  "description": "test",
-   "main": "index.js",
-   "scripts": {
-     "version": "node version.js",
-`;
+// the *entire* stdout and stderr (buffered)
+//   console.log(`stdout: ${stdout}`);
+let x = "*************************";
+let pos = stdout.replace(/@@[\n\s\S]*?@@/, x);
+let res = pos
+  .substring(pos.indexOf(x) + x.length + 1)
+  .replace(/@@[\n\s\S]*?@@/, "");
+console.log(
+  res.match(/\+.*/g).reduce((result, current) => {
+    return result + "\n" + current.substring(1);
+  }, "")
+);
+//   console.log(`stderr: ${stderr}`);
+// }
